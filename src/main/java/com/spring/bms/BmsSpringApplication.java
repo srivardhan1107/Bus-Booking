f5ca94d20ab482b17b185f3a35f6d34b198f1191
srivardhan1107 <srivardhan20042@gmail.com>
2026-05-06 22:36:24 +0530

    initial setup

---
+++ src/main/java/com/spring/bms/BmsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/bms/BmsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
