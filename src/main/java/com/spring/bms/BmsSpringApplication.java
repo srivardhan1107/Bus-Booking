3063cf7327b5773154ff322a5fa3afb92773d0e6
saimaheshmarati <msaimahesh26@gmail.com>
2026-05-07 14:10:37 +0530

    changed some data

--- src/main/java/com/spring/bms/BmsSpringApplication.java
+++ src/main/java/com/spring/bms/BmsSpringApplication.java
@@ -1,7 +1,13 @@
 package com.spring.bms;
 
+import com.spring.bms.entity.Role;
+import com.spring.bms.entity.User;
+import com.spring.bms.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BmsSpringApplication {
@@ -10,4 +16,19 @@ public class BmsSpringApplication {
 		SpringApplication.run(BmsSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (userRepository.findByEmail("admin@bms.com").isEmpty()) {
+				User admin = User.builder()
+						.name("Admin")
+						.email("admin@bms.com")
+						.password(passwordEncoder.encode("admin123"))
+						.role(Role.ADMIN)
+						.build();
+				userRepository.save(admin);
+				System.out.println("Seeded Default Admin User: admin@bms.com / admin123");
+			}
+		};
+	}
 }
